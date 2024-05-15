3b5a16d771d4141fef8f8a7a9b743dab6b884514
Taisiia <taisionok@gmail.com>
2024-05-15 12:49:14 +0200

    start

--- src/main/java/org/example/_20240515/Task2.java
+++ src/main/java/org/example/_20240515/Task2.java
@@ -10,28 +10,31 @@ public class Task2 {
         //short: 56
         //float: 4.7333436
         //double: 4.355453532
+        Double doubleObj = 432.542;
+
         //long: 12121
         //
         //Character: G
+        Character charObj = 'G';
+
         //Integer: 89
+        Integer integerObj = 89;
+
         //Byte: 4
         Byte byteObj = 4;
+
         //Short: 56
+        Short shortObj = 56;
+
         //Float: 4.7333436
+        Float floatObj = 4.7333436F;
+
         //Double: 4.355453532
+
+
         //Long: 12121
+        Long longObj = 12121L;
 
-
-
-
-        Short shortObj = 600;
-        Integer integerObj = 100000;
-        Long longObj = 1_000_000_000L;
-
-        Float floatObj = 15.1234f;
-        Double doubleObj = 432.542;
-
-        Character charObj = 'Y';
         Boolean booleanObj = true;
     }
 }
